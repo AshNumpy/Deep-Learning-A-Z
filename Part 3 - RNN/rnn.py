7ca335428a9e0d1aa7b440e5c52bf3bf0a91d7c7
ramazanerduran <ramazan.erduran@outlook.com.tr>
2024-05-11 20:16:15 +0300

    Recurrent neural network python file

--- Part 3 - RNN/rnn.py
+++ Part 3 - RNN/rnn.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the training set
-dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
+dataset_train = pd.read_csv('./Part 3 - RNN/Google_Stock_Price_Train.csv')
 training_set = dataset_train.iloc[:, 1:2].values
 
 # Feature Scaling
@@ -21,7 +21,7 @@ training_set_scaled = sc.fit_transform(training_set)
 # Creating a data structure with 60 timesteps and 1 output
 X_train = []
 y_train = []
-for i in range(60, 1258):
+for i in range(60, len(training_set_scaled)):
     X_train.append(training_set_scaled[i-60:i, 0])
     y_train.append(training_set_scaled[i, 0])
 X_train, y_train = np.array(X_train), np.array(y_train)
